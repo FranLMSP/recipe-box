e7c68bfe5927b72ff00aadd69d2cfd8b0a8e8eb9
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-21 02:47:55 -0400

    Login and register finished

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -18,6 +18,28 @@
 			</ul>
 		</div>
 
+		<div class="flash flash__success" v-if="flash.success">
+			{{ flash.success }}
+		</div>
+
+		<div class="flash flash__error" v-if="flash.error">
+			{{ flash.error }}
+		</div>
+
 		<router-view></router-view>
 	</div>
 </template>
+
+<script type="text/javascript">
+	
+	import Flash from './helpers/flash'
+
+	export default {
+		data() {
+			return {
+				flash: Flash.state
+			}
+		}
+	}
+
+</script>
